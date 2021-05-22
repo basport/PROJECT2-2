62d54fc524d74c1e328ffa63095ffd89147b452e
basport <basport2410@ukr.net>
2021-05-22 21:42:01 +0300

    14

--- PROJECT2-2/ViewController.swift
+++ PROJECT2-2/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var rSlider: UISlider! {
+        didSet{
+            rSlider.tintColor = UIColor.red
+        }
+    }
+    @IBOutlet weak var gSlider: UISlider!{
+        didSet{
+            gSlider.tintColor = UIColor.green
+        }
+    }
+    @IBOutlet weak var bSlider: UISlider!{
+        didSet{
+            bSlider.tintColor = UIColor.blue
+        }
+    }
+    
+    @IBOutlet weak var rLabel: UILabel!
+    @IBOutlet weak var gLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func changeColor() {
+    
+        colorView.backgroundColor = UIColor(displayP3Red: CGFloat(rSlider.value), green: CGFloat(gSlider.value), blue: CGFloat(bSlider.value), alpha: 1)
+    
+    }
 
-
+    @IBAction func rgbSliderChanged(_ sender: Any) {
+        
+        rLabel.text = String(format: "%.2f", rSlider.value)
+        gLabel.text = String(format: "%.2f", gSlider.value)
+        bLabel.text = String(format: "%.2f", bSlider.value)
+        
+        changeColor()
+    }
+    
 }
 
